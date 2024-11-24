a8270471b503a8c35f30dbea8778e514072ba3f3
sudhirvacharya <sudheerpaniyur@gmail.com>
2024-11-24 21:34:44 +0530

    added intially file, but facing issu in compilation

---
+++ RS485/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace RS485
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtBaud = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnconnect = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(181, 43);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 26);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.Text = "COM2";
+            // 
+            // txtBaud
+            // 
+            this.txtBaud.Location = new System.Drawing.Point(181, 91);
+            this.txtBaud.Name = "txtBaud";
+            this.txtBaud.Size = new System.Drawing.Size(100, 26);
+            this.txtBaud.TabIndex = 3;
+            this.txtBaud.Text = "9600";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(95, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "BaudRate";
+            // 
+            // btnconnect
+            // 
+            this.btnconnect.Location = new System.Drawing.Point(91, 145);
+            this.btnconnect.Name = "btnconnect";
+            this.btnconnect.Size = new System.Drawing.Size(190, 31);
+            this.btnconnect.TabIndex = 4;
+            this.btnconnect.Text = "Connect";
+            this.btnconnect.UseVisualStyleBackColor = true;
+            this.btnconnect.Click += new System.EventHandler(this.btnconnect_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(151, 329);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(113, 20);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "Not connected";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 329);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnconnect);
+            this.Controls.Add(this.txtBaud);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtBaud;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnconnect;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ RS485/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EasyModbus;
+using NModbus;
+using NModbus.Device;
+
+
+namespace RS485
+{
+    
+    public partial class Form1 : Form
+    {
+        ModbusClient ModClient = new ModbusClient(); 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnconnect_Click(object sender, EventArgs e)
+        {
+            if (btnconnect.Text == "Connect")
+            {/*
+                ModClient.SerialPort = txtPort.Text;
+                ModClient.Baudrate = int.Parse(txtBaud.Text);
+                ModClient.Parity = Parity.None;
+                ModClient.StopBits = StopBits.Two;
+                
+                ModClient.Connect();
+                // Create a Modbus master
+                var master = ModbusMaster.CreateRtu(ModClient);
+
+                // Read the PV value (adjust the slave address and register address as needed)
+                ushort pvValue = master.ReadHoldingRegisters(1, 40001, 1)[0];
+                lblStatus.Text = "Connected";
+                btnconnect.Text = "DisConnect";
+                */
+                SerialPort serialPort = new SerialPort("COM1", 9600, Parity.None, 8, StopBits.One);
+                serialPort.Open();
+
+
+                var master = ModbusMaster.CreateRtu(serialPort);
+                lblStatus.Text = "Connected";
+                btnconnect.Text = "DisConnect";
+            }
+            else
+            {
+                ModClient.Disconnect();
+                lblStatus.Text = "DisConnected";
+                btnconnect.Text = "Connect";
+            }
+
+        }
+    }
+}
